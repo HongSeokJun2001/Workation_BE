9c1420de3afdb0fb30385c0402f177986617e2a7
Sword672 <siteblod@gmail.com>
2026-09-02 16:50:11 +0900

    경로수정

--- src/main/java/com/kh/workation/config/WebConfig.java
+++ src/main/java/com/kh/workation/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.kh.workation.config;
 
 import java.io.File;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,14 +13,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload-dir}")
+    private String uploadDir;
+
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		
-		// 1. C드라이브 실제 uploads 폴더의 File 객체 생성
-        File uploadDir = new File("C:/Final_Project/Workation_BE/uploads/");
+        // application.properties에 설정된 실제 uploads 폴더의 File 객체 생성
+        File uploadDirectory = new File(uploadDir);
         
-        // 2. URI 형태(file:///C:/Final_Project/Workation_BE/uploads/)로 자동 변환
-        String uploadPath = uploadDir.toURI().toString();
+        // URI 형태로 자동 변환
+        String uploadPath = uploadDirectory.toURI().toString();
 
         // /uploads/** URL 요청을 실제 로컬 uploads 폴더로 연결
         registry.addResourceHandler("/uploads/**")
